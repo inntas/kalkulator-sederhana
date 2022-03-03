16baba8ff8c2b36d816a6b3ac88246a197f03497
Nur Innas Tassya <inntas9901@gmail.com>
2022-03-03 13:55:50 +0700

    percabangan validasi selesai

--- script.js
+++ script.js
@@ -6,6 +6,20 @@ const result = document.getElementById('result');
 addButton.addEventListener('click', () =>{
     const inputOneValue = inputOne.value;
     const inputTwoValue = inputTwo.value;
-    const addedResult = parseInt(inputOneValue) + parseInt(inputTwoValue);
-    result.innerHTML = addedResult;
+    if(validation(inputOneValue, inputTwoValue) === true){
+        const addedResult = parseInt(inputOneValue) + parseInt(inputTwoValue);
+        result.innerHTML = addedResult;
+    }else {
+        alert('Input tidak valid');
+    }
+
+    
 });
+
+const validation = (inputOne, inputTwo) =>{
+    if((inputOne == false) || (inputTwo == false) || (isNaN(inputOne)) || (isNaN(inputTwo))){
+        return false;     
+    }else{
+        return true;
+    }
+}
